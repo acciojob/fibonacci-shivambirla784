74875b98c6b081bcc400b94a84dbd903e535f774
shivambirla784 <86475649+shivambirla784@users.noreply.github.com>
2024-09-12 23:11:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 function fibonacci(num) {
-// your code here
-	if(num<2) return num;
-	return fibonacci(num-1) + fibonacci(num-2);
-	
+  if (num === 0) return 0;
+  if (num === 1) return 0;
+  if (num === 2) return 1;
+  
+  let a = 0, b = 1;
+  for (let i = 3; i <= num; i++) {
+    let next = a + b;
+    a = b;
+    b = next;
+  }
+  return b;
 }
 
 module.exports = fibonacci;
